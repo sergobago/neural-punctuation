de38150570d7f53a6380324a6488851eaa9c354f
Sergey <sergobago1000k@mail.ru>
2025-01-28 13:58:34 +0300

    Update spacy.py

--- constants/spacy.py
+++ constants/spacy.py
@@ -3,7 +3,7 @@ from os.path import join
 from constants.project_params import NMODELS_DIR
 
 SPACY_DIR_NAME = 'spacy'
-SPACY_CPU_CORES = 4
+SPACY_CPU_CORES = 1
 MAX_SPACY_CHUNK_SIZE = int(1e7)
 SPACY_DIR = join(NMODELS_DIR, SPACY_DIR_NAME)
 
